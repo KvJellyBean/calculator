0c7f9bdf2bf91da39d29a769cf58b218c398dcd2
KvJellyBean <kevinnatan91@gmail.com>
2023-11-24 14:49:28 +0700

    Make the result fixed 2 number behind comma

--- scripts/script.js
+++ scripts/script.js
@@ -10,11 +10,14 @@ function subtract(operand1, operand2) {
 }
 
 function multiply(operand1, operand2) {
-    return operand1 * operand2;
+    return (operand1 * operand2).toFixed(2);
 }
 
 function divide(operand1, operand2) {
-    return operand1 / operand2;
+    if (operand2 == 0) {
+        return 'Error';
+    }
+    return (operand1 / operand2).toFixed(2);
 }
 
 function operate(operand1, operator, operand2) {
